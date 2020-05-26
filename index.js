5aa810904eea80be0a2b4ea86d2acde396185ee9
azcvcza <283940802@qq.com>
2020-05-26 23:09:42 +0800

    finish whole js

--- index.js
+++ index.js
@@ -125,3 +125,112 @@ colors = [
     "#FF9800",
     "#FF5722"
   ];
+
+  function particle(x,y,type){
+      this.radius = 1.1;
+      this.futurRadius = utils.randomInt(radius,radius+3);
+      this.rebond = utils.randomInt(1,5);
+      this.x = x;
+      this.y = y;
+      this.dying = false;
+      this.base = [x,y];
+      this.vx = 0;
+      this.vy = 0;
+      this.type = type;
+      this.friction = 0.99;
+      this.gravity = gravity;
+      this.color = colors[Math.floor(Math.random()*colors.length)];
+        
+      this.getSpeed = function(){
+          return Math.sqrt(this.vx*this.vx + this.vy*this.vy);
+      }
+      this.setSpeed = function(speed){
+          var heading = this.getHeading();
+          this.vx = Math.cos(heading)*speed;
+          this.vy = Math.sin(heading)*speed;
+
+      }
+      this.getHeading = function(){
+          return Math.atan2(this.vy,this.vx);
+      }
+      this.setHeading = function(){
+          var speed = this.getSpeed();
+          this.vx = Math.cos(heading)*speed;
+          this.vy = Math.sin(heading)*speed;
+
+      }
+      this.angleTo = function(p2){
+          return Math.atan2(p2.y - this.y, p2.x-this.x);
+      }
+      this.update = function(heading){
+          this.x += this.vx;
+          this.y += this.vy;
+          this.vy += gravity;
+          this.vx *= this.friction;
+          this.vy *= this.friction;
+          if(this.radius < this.futurRadius && this.dying === false){
+              this.radius += duration;
+          }else{
+              this.dying = true;
+          }
+
+          if(this.dying === true){
+              this.radius -= duration;
+          }
+          ctx.beginPath();
+          ctx.fillStyle = this.color;
+          ctx.arc(this.x,this.y,this.radius, Math.PI*2,false);
+          ctx.fill();
+          ctx.closePath();
+
+          if(this.y<0 || this.radius<1){
+              this.x = this.base[0];
+              this.dying = false;
+              this.y = this.base[1];
+              this.radius = 1.1;
+              this.setSpeed(speed);
+              this.futurRadius = utils.randomInt(radius, radius+3);
+              this.setHeading(utils.randomInt(utils.degreeToRads(0),utils.degreeToRads(360)))
+          }
+      }
+
+      this.setSpeed(utils.randomInt(0.1,0.5));
+      this.setHeading(utils.randomInt(utils.degreeToRads(0),utils.degreeToRads(360)));
+     
+  }
+  element2 = document.getElementById('2')
+  element3 = document.getElementById('3')
+  element4 = document.getElementById('4')
+  element5 = document.getElementById('5')
+  element6 = document.getElementById('6')
+  fieldValue = document.getElementById('message');
+  gravity = parseFloat(element2.value);
+  duration = parseFloat(element3.value);
+  resolution = parseFloat(element4.value);
+  speed = parseFloat(element5.value);
+  radius = parseFloat(element5.value);
+
+  var message = new shape(W/2,H/2+50,fieldValue.value);
+  message.getValue();
+  
+  function change(){
+      ctx.clearRect(0,0,W,H);
+     gridX = parseFloat(element4.value);
+     gridY = parseFloat(element4.value);
+     message.placement = [];
+     message.text = fieldValue.value;
+     message.getValue();
+  }
+  function changeV(){
+      gravity = parseFloat(element2.value);
+      duration = parseFloat(element3.value);
+      speed = parseFloat(element5.value);
+      radius = parseFloat(element6.value);
+  }
+  function update(){
+      ctx.clearRect(0,0,W,H);
+      for(var i=0;i<message.placement.length;i++){
+          message.placement[i].update();
+      }
+      requestAnimationFrame(update);
+  }
